ca1384030b12a0d85be95d8d1c6a5328b2f553d2
handikoswara <39146228+handikoswara@users.noreply.github.com>
2021-07-10 18:52:53 +0700

    User Login Reducer & Action

--- frontend/src/screens/CartScreen.tsx
+++ frontend/src/screens/CartScreen.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { Row, Col, ListGroup, Image, Form, Button, Card } from "react-bootstrap";
 import Message from "../components/Message";
-import { addToCart, removeFromCart } from "../actions/cardActions";
+import { addToCart, removeFromCart } from "../actions/cartActions";
 
 const CartScreen = ({ match, location, history }: any) => {
     const productId = match.params.id;
