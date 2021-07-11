30707c16fc90ac422c7f252e49ff0729ec6ec147
handikoswara <39146228+handikoswara@users.noreply.github.com>
2021-07-11 09:20:04 +0700

    Show User In Navbar & Logout

--- frontend/src/actions/userActions.ts
+++ frontend/src/actions/userActions.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../constants/userConstants";
+import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT } from "../constants/userConstants";
 
 export const login = (email: any, password: any) => async (dispatch: any) => {
     try {
@@ -24,4 +24,12 @@ export const login = (email: any, password: any) => async (dispatch: any) => {
     } catch (error) {
         dispatch({ type: USER_LOGIN_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message })
     }
+}
+
+export const logout = () => (dispatch: any) => {
+    localStorage.removeItem("userInfo");
+
+    dispatch({
+        type: USER_LOGOUT
+    })
 }
